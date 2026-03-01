f42106238e81ed8a53f1e47bdc8c2cf9bf81c7d3
Mark Joseph Bulan <markjosephpbulan@gmail.com>
2026-03-02 01:52:57 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+// config.js - Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCe2b9DplixnglV8uESPMUmaiL3InNYhm0",
+  authDomain: "esp32-iot-c4b7a.firebaseapp.com",
+  databaseURL: "https://esp32-iot-c4b7a-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "esp32-iot-c4b7a",
+  storageBucket: "esp32-iot-c4b7a.firebasestorage.app",
+  messagingSenderId: "360877862268",
+  appId: "1:360877862268:web:7ad2b6286290dad84908f9"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
